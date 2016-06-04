f94ac2d20a53dfa8cc0ac34a8995e7b6e7998530
Hikki <hikkihiki@gmail.com>
2016-06-05 02:24:02 +0800

    Add query path params.

--- crawler/lib/app_token.js
+++ crawler/lib/app_token.js
@@ -1,16 +1,19 @@
 'use strict'
 
 var FB = require('fb');
+var Step = require('step');
 
 module.exports.acquire = (callback) => {
-    FB.api('oauth/access_token', {
-        client_id: '1043951495691711',
-        client_secret: '77f3368654e47192d7eb0f132cbe00e3',
-        grant_type: 'client_credentials'
-    }, function(res) {
-        if (!res || res.error) {
-            callback(null, res);
+    Step(
+        function acquire() {
+            FB.napi('oauth/access_token', {
+                client_id: '1043951495691711',
+                client_secret: '77f3368654e47192d7eb0f132cbe00e3',
+                grant_type: 'client_credentials'
+            }, this)
+        },
+        function process(err, res) {
+            callback(err, res && !err ? res.access_token : null);
         }
-        callback(res.access_token, null);
-    });
+    );
 }
--- crawler/page/handler.js
+++ crawler/page/handler.js
@@ -4,18 +4,15 @@ var FB = require('fb');
 var app_token = require('../lib/app_token');
 
 module.exports.handler = function(event, context, callback) {
-    app_token.acquire((token, err) => {
+    app_token.acquire((err, res) => {
         if (err) {
             context.done(err, null);
             return;
         }
-        FB.setAccessToken(token);
-        FB.api('/medium', function(res) {
-            if (res && res.error) {
-                context.done(res.error, null);
-            } else {
-                context.done(null, res);
-            }
+
+        FB.setAccessToken(res);
+        FB.napi(`/${event.pageId}/posts?fields=name,shares`, function(err, res) {
+            context.done(err, res);
         });
     });
 };
